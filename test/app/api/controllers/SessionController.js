a1011d11ce84327b2d0e397ff19969b1c12d2939
csrgxtu <1246506786@qq.com>
2015-03-27 16:27:08 +0800

    session worked for the project

--- test/app/api/controllers/SessionController.js
+++ test/app/api/controllers/SessionController.js
@@ -27,4 +27,87 @@ module.exports = {
       return res.send(sails.config.returnCode.QUERY_OK);
     });
   },
+
+  /**
+   * create
+   * craate an record
+   *
+   * @param UserId string
+   * @param LoginAndroid string
+   * @return Json Data.
+   */
+  create: function(req, res) {
+    var UserId = req.param('UserId');
+    var LoginAndroid = req.param('LoginAndroid');
+    if (!UserId || !LoginAndroid) {
+      return res.send(sails.config.returnCode.INVALID_PARAM);
+    }
+
+    XFJWT_Session
+      .create({
+        UserId: UserId,
+        LoginAndroid: LoginAndroid
+      }, function cb(err, recs) {
+        if (err) {
+          sails.config.returnCode.DB_ERROR.data = err;
+          return res.send(sails.config.returnCode.DB_ERROR);
+        }
+
+        sails.config.returnCode.QUERY_OK.data = recs;
+        return res.send(sails.config.returnCode.QUERY_OK);
+      });
+  },
+
+  /**
+   * read
+   * read records
+   *
+   * @param pageIndex int
+   * @param pageSize int
+   * @return Json Data.
+   */
+  read: function(req, res) {
+    var pageIndex = req.param('pageIndex');
+    var pageSize = req.param('pageSize');
+    if (!pageIndex || !pageSize) {
+      return res.send(sails.config.returnCode.INVALID_PARAM);
+    }
+
+
+    XFJWT_Session
+      .find({})
+      .exec(function cb(err, recs) {
+        if (err) {
+          sails.config.returnCode.DB_ERROR.data = err;
+          return res.send(sails.config.returnCode.DB_ERROR);
+        }
+
+        sails.config.returnCode.QUERY_OK.data = recs;
+        return res.send(sails.config.returnCode.QUERY_OK);
+      });
+  },
+
+  /**
+   * update
+   * update record
+   *
+   * @param id string
+   * @param UserId string
+   * @param Loginandroid string
+   * @return Json Data.
+   */
+  update: function(req, res) {
+
+  },
+
+  /**
+   * delete
+   * delete the record
+   *
+   * @param id string
+   * @return Json Data
+   */
+  delete: function(req, res) {
+
+  }
 }
